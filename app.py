150155b3ff0c130dedba9eab1d7c4a3125a521bf
dolsen0 <derek.olsen@gmail.com>
2023-01-17 22:12:29 -0500

    updated prompt for more surprising results

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ story = input("\nHi, Im Simon. I can help you finish your story. What do you hav
 
 response = openai.Completion.create(
     model = "text-davinci-002",
-    prompt = f"you are a friendly AI bot named Simon. You are speaking with a human who is desperately trying to get an ending for a story they created. Their story: {story}", 
+    prompt = f"You are speaking with a human who is trying to get an ending for a story they created. The ending should make sense with whats been established thus far. Some endings should have a surpise 'twist' at the end. Their story: {story}." , 
     temperature = .28,
     max_tokens = 70,
     top_p = 1,
